bc0d92a9aee2c563dae64f78d053671fa081a796
baguzzza <bagusracmad@gmail.com>
2021-10-11 23:31:11 +0700

    Update Management and Upload Management CMS Yowez

--- Apps/app.js
+++ Apps/app.js
@@ -3,7 +3,7 @@ var __env = {};
 if (window) {
     Object.assign(__env, window.__env);
 }
-app = angular.module("home", ["ngRoute"]);
+app = angular.module("home", ["ngRoute", 'ngFileUpload']);
 app.constant('__env', __env);
 app.config(function ($logProvider) {
     $logProvider.debugEnabled(__env.debugEnabled);
@@ -49,7 +49,7 @@ app.config(function ($routeProvider, $locationProvider) {
     });
 });
 
-app.factory("httpRequest", function ($http) {
+app.factory("httpRequest", function ($http, Upload) {
     return {
         get: function (url, params, token) {
             return $http({
@@ -118,6 +118,23 @@ app.factory("httpRequest", function ($http) {
                 return error;
             });
         },
+        upload: function (url, token, form) {
+            return Upload.upload({
+                url: (__env.debugEnabled == false) ? __env.devApiUrl + url : __env.apiUrl + url,
+                headers: {
+                    Authorization: (token != undefined) ? "Bearer " + token : "",
+                    Accept: "application/json",
+                },
+                data: form
+            }).then(function (resp) {
+                return resp;
+            }, function (resp) {
+                return resp;
+            }, function (evt) {
+                var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                return progressPercentage;
+            });
+        }
     };
 });
 
